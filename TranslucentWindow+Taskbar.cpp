66c16389e36ac421875255018d41d15742e71fce
Muhammad Azeem <azeem.rana85@gmail.com>
2020-04-06 00:39:25 +0500

    CLI Menu Added (Temporary)

--- TranslucentWindow+Taskbar.cpp
+++ TranslucentWindow+Taskbar.cpp
@@ -1,6 +1,7 @@
 #include <Windows.h>
 #include <iostream>
 #include <string>
+#include <vector>
 
 #define ACCENT_ENABLE_TRANSPARENTGRADIENT 2
 
@@ -16,6 +17,13 @@ struct WINCOMPATTRDATA {
 	PVOID pData;
 	ULONG ulDataSize;
 };
+
+std::vector<HWND> handles;
+
+BOOL CALLBACK collect(HWND handle, LPARAM lParam) {
+	handles.push_back(handle);
+	return true;
+}
 
 
 void SetBlurBehind(HWND hwnd) {
@@ -33,7 +41,8 @@ void SetBlurBehind(HWND hwnd) {
 			// Only works on Win10
 			if (SetWindowCompositionAttribute) {
 				ACCENTPOLICY policy =
-				{3, 0 , 3 == (ACCENT_ENABLE_TRANSPARENTGRADIENT) ? 255 : 0,0 };
+				/*{ 4, 0 , 3 == (ACCENT_ENABLE_TRANSPARENTGRADIENT) ? 255 : 26, 0 };*/
+				{ 2, 0 , 2 == (ACCENT_ENABLE_TRANSPARENTGRADIENT) ? 255 : 0, 0 };
 				WINCOMPATTRDATA data = { 19,&policy, sizeof(ACCENTPOLICY) };
 				SetWindowCompositionAttribute(hwnd, &data);
 			}
@@ -45,31 +54,59 @@ void SetBlurBehind(HWND hwnd) {
 	}
 }
 
+std::vector<std::wstring> getWindows() {
+	std::vector<std::wstring> titles;
+	EnumDesktopWindows(NULL, collect, reinterpret_cast<LPARAM>(&titles));
+	return titles;
+}
+
 int main() {
 
 	HWND TaskbarHandle = FindWindow("Shell_TrayWnd", NULL);
 	//HWND SecondaryTaskbar = FindWindow("This PC", NULL);
 	HWND Actionbar = GetDesktopWindow();
 	Sleep(3000);
-	HWND pc = GetTopWindow(Actionbar);
+	
 
-	HWND c = GetForegroundWindow();
-	tagWINDOWINFO *inf = new tagWINDOWINFO;
-	inf->cbSize = sizeof(WINDOWINFO);
+	getWindows();
 
-	GetWindowInfo(c, inf);
 
-	LPSTR *a = new LPSTR;
-	SendMessage(c, WM_GETTEXT, NULL, NULL);
 
-	//ShowWindow(c, SW_SHOW);
-	//std::string p = *a;
-	SetBlurBehind(c);
+	for (const auto& hwnd : handles) {
+		const DWORD TITLE_SIZE = 1024;
+		WCHAR windowTitle[TITLE_SIZE];
+		int length = ::GetWindowTextLength(hwnd);
+		if (IsWindowVisible(hwnd) && length > 0) {
+			GetWindowTextW(hwnd, windowTitle, TITLE_SIZE);
+			std::wcout << L"Title: " << windowTitle << std::endl;
+		}
+	}
+
+	std::string t;
+
+	do {
+		std::cout << "\nEnter the Window Title: ";
+		std::getline(std::cin, t);
+		HWND pc = FindWindowA(NULL, t.c_str());
+
+		Sleep(1000);
+
+		//HWND c = GetForegroundWindow();
+		//tagWINDOWINFO *inf = new tagWINDOWINFO;
+		//inf->cbSize = sizeof(WINDOWINFO);
+
+		//GetWindowInfo(c, inf);
+
+		//LPSTR *a = new LPSTR;
+		//SendMessage(c, WM_GETTEXT, NULL, NULL);
+
+		//ShowWindow(c, SW_SHOW);
+		//std::string p = *a;
+		SetBlurBehind(pc);
+
+	} while (t != "...");
 	SetBlurBehind(TaskbarHandle);
 
 	std::cout << "Hello!";
-
-
-
 
 }
